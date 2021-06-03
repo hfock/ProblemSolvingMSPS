e36cf2ffc9270ceabcc80a333629a18f9e814bca
Patrick Malik <e11776819@student.tuwien.ac.at>
2021-06-03 21:34:38 +0200

    Adds basic project setup, sample instances and the instance class

---
+++ msps/model/instance.py
@@ -0,0 +1,20 @@
+class Instance:
+
+    def __init__(self, mint, maxt, nActs, dur, nSkills, sreq, nResources, mastery, nPrecs, pred, succ, nUnrels, unpred,
+                 unsucc, useful_res, potential_act):
+        self.mint = mint
+        self.maxt = maxt
+        self.nActs = nActs
+        self.dur = dur
+        self.nSkills = nSkills
+        self.sreq = sreq
+        self.nResources = nResources
+        self.mastery = mastery
+        self.nPrecs = nPrecs
+        self.pred = pred
+        self.succ = succ
+        self.nUnrels = nUnrels
+        self.unpred = unpred
+        self.unsucc = unsucc
+        self.useful_res = useful_res
+        self.potential_act = potential_act
