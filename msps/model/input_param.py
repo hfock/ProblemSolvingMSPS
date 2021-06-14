a82a459b828a076da1b02d5c300e89a63e89344e
Patrick Malik <e11776819@student.tuwien.ac.at>
2021-06-14 19:57:39 +0200

    not sure what this is

--- msps/model/input_param.py
+++ msps/model/input_param.py
@@ -1,6 +1,7 @@
 class InputParam:
 
-    def __init__(self, t_stop=None, t_inital=None, termination_condition=None, t_value_change=None):
+    def __init__(self, param_number=None, t_stop=None, t_inital=None, termination_condition=None, t_value_change=None):
+        self.param_number = param_number
         self.t_stop = t_stop
         self.t_initial = t_inital
         self.termination_condition = termination_condition
@@ -8,6 +9,7 @@ class InputParam:
 
     def __str__(self) -> str:
         string = "{\n"
+        string += "\tparam_number: " + str(self.param_number) + ",\n"
         string += "\tt_stop: " + str(self.t_stop) + ",\n"
         string += "\tt_inital: " + str(self.t_initial) + ",\n"
         string += "\ttermination_condition: " + str(self.termination_condition) + ",\n"
